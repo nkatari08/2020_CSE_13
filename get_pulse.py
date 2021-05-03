cfa47aa14dbe1805343578495b13b56beb49a2f4
nkatari08 <41177877+nkatari08@users.noreply.github.com>
2021-05-03 12:22:57 +0530

    Add files via upload

---
+++ get_pulse.py
@@ -0,0 +1,28 @@
+from lib.device import Camera
+from lib.processors_noopenmdao import findFaceGetPulse
+from lib.interface import plotXY, imshow, waitKey, destroyWindow
+from cv2 import moveWindow
+import argparse
+import numpy as np
+import datetime
+import socket
+import sys
+
+
+ def toggle_cam(self):
+        if len(self.cameras) > 1:
+            self.processor.find_faces = True
+            self.bpm_plot = False
+            destroyWindow(self.plot_title)
+            self.selected_cam += 1
+            self.selected_cam = self.selected_cam % len(self.cameras)
+
+ def write_csv(self):
+        """
+        Writes current data to a csv file
+        """
+        fn = "Webcam-pulse" + str(datetime.datetime.now())
+        fn = fn.replace(":", "_").replace(".", "_")
+        data = np.vstack((self.processor.times, self.processor.samples)).T
+        np.savetxt(fn + ".csv", data, delimiter=',')
+        print("Writing csv")
